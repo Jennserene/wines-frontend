e9b2cf59f561795c97ec99777401abda8ac42700
Jennserene <jennserene@gmail.com>
2021-08-20 12:22:22 -0400

    Added app title

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h1>App</h1>
         <HashRouter>
           <div>
             <Route exact path="/" component={HomePage} />
